62cf937f55d7c90b95939537997160030653b39b
Tri Vuong <trivektor@gmail.com>
2011-02-08 23:17:18 -0500

    ResumeWorkExperience can now be persisted

--- app/controllers/resume_work_experiences_controller.rb
+++ app/controllers/resume_work_experiences_controller.rb
@@ -8,6 +8,12 @@ class ResumeWorkExperiencesController < ApplicationController
   end
   
   def create
+    @resume_work_experience = ResumeWorkExperience.new(params[:resume_work_experience])
+    @resume_work_experience.resume_id = params[:resume_id]
+    
+    if @resume_work_experience.save
+      redirect_to :controller => :resumes, :action => :edit, :id => params[:resume_id]
+    end
     
   end
   
--- test/unit/resume_work_experience_test.rb
+++ test/unit/resume_work_experience_test.rb
@@ -21,12 +21,11 @@ class ResumeWorkExperienceTest < ActiveSupport::TestCase
     
     resume = Resume.create(:url => "trivektor", :title => "Tri Vuong's resume")
     
-    resume_work_experience = ResumeWorkExperience.new(:organization_name => "", 
-    :begin_date => "", :end_date => "", :resume_id => resume.id)
+    resume_work_experience = ResumeWorkExperience.new(:organization_name => "Organzation", 
+    :begin_date => "Begin Date", :end_date => "End Date", :resume_id => resume.id)
     
-    resume_work_experience.save
-    
-    
+    assert_equal true, resume_work_experience.save
+    assert_equal resume.id, resume_work_experience.resume_id
     
   end
   
