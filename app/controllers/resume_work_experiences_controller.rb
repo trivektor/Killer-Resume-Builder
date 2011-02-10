bd68ed5ce9b525cab2f40e4efe28289ae1405893
Tri Vuong <trivektor@gmail.com>
2011-02-09 20:18:07 -0500

    ResumeWorkExperience can now be edited and persisted

--- app/controllers/resume_work_experiences_controller.rb
+++ app/controllers/resume_work_experiences_controller.rb
@@ -17,4 +17,35 @@ class ResumeWorkExperiencesController < ApplicationController
     
   end
   
+  def edit
+    if manipulatable? params[:resume_id]
+      @resume_work_experience = find_resume_work_experience params[:id], params[:resume_id]
+    end
+  end
+  
+  def update
+    if manipulatable? params[:resume_id]
+      resume_work_experience = find_resume_work_experience params[:id], params[:resume_id]
+      if resume_work_experience.update_attributes(params[:resume_work_experience])
+        flash[:notice] = "Work experience has been updated"
+        redirect_to :controller => :resume_work_experiences, :action => :edit, :id => params[:id]
+      end
+    end
+  end
+  
+  def manipulatable?(resume_id)
+    resume = find_resume resume_id
+    resume.user_id == current_user.id 
+  end
+  
+  private
+  
+  def find_resume(resume_id)
+    Resume.find(resume_id)
+  end
+  
+  def find_resume_work_experience(resume_work_experience_id, resume_id)
+    ResumeWorkExperience.where(:id => resume_work_experience_id, :resume_id => resume_id).first
+  end
+  
 end
