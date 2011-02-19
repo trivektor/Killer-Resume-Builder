e0375995ce88b7ff208ba11569a20e63bcea30fd
Tri Vuong <trivektor@gmail.com>
2011-02-19 08:39:56 -0500

    Order resumes by date

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   
   def index
     @body_id = "dashboard"
-    @resumes = Resume.where("user_id = ?", current_user.id)
+    @resumes = Resume.where("user_id = ?", current_user.id).order("created_at DESC")
   end
   
 end
