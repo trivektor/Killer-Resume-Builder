c58d68514941a0c1227c4615f33426a2cc98dc7f
Tri Vuong <trivektor@gmail.com>
2011-02-26 13:44:42 -0500

    Use attr_accessor in User model for signup form for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     
     if @user.save
-      @user.profile = Profile.new
+      @user.profile = Profile.new(:first_name => params[:user][:first_name], :last_name => params[:user][:last_name])
       @user.profile.save
       
       redirect_to dashboard_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 class User < ActiveRecord::Base
   
-  validates_length_of :username, :minimum => 4, :message => " is too short"
-  validates :email, :email => {:message => " is not valid"}
-  validates_uniqueness_of :email
-  validates_uniqueness_of :username
-  validates_length_of :password, :minimum => 4, :message => " is too short"
-  
   has_one :profile, :dependent => :destroy
   has_many :resumes, :dependent => :destroy
   has_many :thoughts, :dependent => :destroy
   has_many :user_threads, :dependent => :destroy
   
   accepts_nested_attributes_for :profile
+  
+  attr_accessor :first_name, :last_name
+  
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_length_of :username, :minimum => 4, :message => " is too short"
+  validates :email, :email => {:message => " is not valid"}
+  validates_uniqueness_of :email
+  validates_uniqueness_of :username
+  validates_length_of :password, :minimum => 4, :message => " is too short"
   
   acts_as_authentic do |config|
     config.crypto_provider = Authlogic::CryptoProviders::MD5
@@ -21,9 +25,5 @@ class User < ActiveRecord::Base
     config.login_field = :email
     config.validate_login_field = false
   end
-  
-  attr_accessible :username, :password, :email
-  
-  
   
 end
