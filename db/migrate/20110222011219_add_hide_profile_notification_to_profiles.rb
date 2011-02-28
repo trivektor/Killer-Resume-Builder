9aa6c553bdc6288b141fc95c387a47a67113240f
Tri Vuong <trivektor@gmail.com>
2011-02-27 23:46:00 -0500

    Fixing migration complaint on heroku

--- db/migrate/20110222011219_add_hide_profile_notification_to_profiles.rb
+++ db/migrate/20110222011219_add_hide_profile_notification_to_profiles.rb
@@ -1,6 +1,6 @@
 class AddHideProfileNotificationToProfiles < ActiveRecord::Migration
   def self.up
-    add_column :profiles, :hide_profile_notification, :boolean, :default => 0, :after => :completeness
+    add_column :profiles, :hide_profile_notification, :boolean, :default => false, :after => :completeness
   end
 
   def self.down
