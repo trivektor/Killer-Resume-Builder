c3aded8cb8384559a2a6ef27da656b46ff002d60
Tri Vuong <trivektor@gmail.com>
2011-02-27 23:44:05 -0500

    Fixing migration complaint on heroku

--- db/migrate/20110220173058_add_allow_sharing_to_resume_settings.rb
+++ db/migrate/20110220173058_add_allow_sharing_to_resume_settings.rb
@@ -1,6 +1,6 @@
 class AddAllowSharingToResumeSettings < ActiveRecord::Migration
   def self.up
-    add_column :resume_settings, :allow_sharing, :boolean, :default => 1, :after => :show_last_updated
+    add_column :resume_settings, :allow_sharing, :boolean, :default => true, :after => :show_last_updated
   end
 
   def self.down
