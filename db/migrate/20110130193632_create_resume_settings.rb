5631c219149da3c21b34a7c7289abba0abef0313
Tri Vuong <trivektor@gmail.com>
2011-02-27 22:59:50 -0500

    Updated migration

--- db/migrate/20110130193632_create_resume_settings.rb
+++ db/migrate/20110130193632_create_resume_settings.rb
@@ -3,7 +3,7 @@ class CreateResumeSettings < ActiveRecord::Migration
     create_table :resume_settings do |t|
       t.integer :resume_id
       t.string :status, :default => "active"
-      t.boolean :hide_personal_info, :default => 0
+      t.boolean :hide_personal_info, :default => false
       t.boolean :alert_copy, :default => 1
       t.boolean :email_notification, :default => 1
       t.boolean :display_personal_photo, :default => 1
