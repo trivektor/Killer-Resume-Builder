f35a1f8a90d317eda5c014b240c17d36ca4c926c
Tri Vuong <trivektor@gmail.com>
2011-03-10 22:47:25 -0500

    Commented out password_reset_instructions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
   
   def password_reset_instructions(user)
-    recipients  "Tri Vuong <tri@trivuong.com>"
-    from        "Killer Resume Builder"
-    subject     "Test"
-    sent_on     Time.now
-    body        { :user => user, :url => "http//www.google.com", :host => "krb.heroku.com" }
+    # recipients  "Tri Vuong <tri@trivuong.com>"
+    #     from        "Killer Resume Builder"
+    #     subject     "Test"
+    #     sent_on     Time.now
+    #     body        { :user => user, :url => "http//www.google.com", :host => "krb.heroku.com" }
   end
   
 end
