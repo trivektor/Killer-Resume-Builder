663f64a1d32fddb1efff6a53d167484e7b9d402e
Tri Vuong <trivektor@gmail.com>
2011-03-28 22:32:08 -0400

    Updated migrations

--- db/migrate/20110324041040_change_reported_type_in_user_threads.rb
+++ db/migrate/20110324041040_change_reported_type_in_user_threads.rb
@@ -1,8 +1,8 @@
 class ChangeReportedTypeInUserThreads < ActiveRecord::Migration
   def self.up
     change_table :user_threads do |t|
-      t.remove_column :reported
-      t.add_column :reported, :boolean, :default => false
+      t.remove :reported
+      t.boolean :reported, :default => false
     end
   end
 
