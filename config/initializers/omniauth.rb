fa73ed4a7d6c131bf7537551ce3eeddb6a995cb3
Tri Vuong <trivektor@gmail.com>
2011-04-12 22:55:30 -0400

    Added Twitter authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, 'yYBcVi6hv17lbixu2jPEA', '8BEbqq7QDExyhtaBqgvchfjwMlQOHQgTdZcxoCvytI'
   provider :facebook, '176795145703361', 'd667b85873b11458373adb8f5d167f6e'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
