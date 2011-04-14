51228f1ef24ed00c2e62959f141d7467cc4a16dc
Tri Vuong <trivektor@gmail.com>
2011-04-13 20:57:38 -0400

    Added LinkedIn authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'yYBcVi6hv17lbixu2jPEA', '8BEbqq7QDExyhtaBqgvchfjwMlQOHQgTdZcxoCvytI'
   provider :facebook, '176795145703361', 'd667b85873b11458373adb8f5d167f6e'
-  #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :linked_in, 'OrNo2OSp34A6hvM4ATiUuqpRW4ysnuALV_gRGBgmPzBghfcidzaKearPlw1YYKAN', 'DfwJf-H-4tZHRLMzgNO4_Ns9vx1Aepm4SSShikDpqashbWAm-C0_FS5VW8AJfHi1'
 end
